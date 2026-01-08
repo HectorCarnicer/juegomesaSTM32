09cb6867f98cccc801ce83fcb624cc15a5bc1032
SaamiiR24 <159050190+SaamiiR24@users.noreply.github.com>
2026-01-08 16:49:03 +0100

    Update game_funcionamiento.c

--- juegomesaSTM32/Core/Src/game_funcionamiento.c
+++ juegomesaSTM32/Core/Src/game_funcionamiento.c
@@ -1,1 +1,57 @@
 
+/* Includes ------*/
+#include "main.h"
+// INCLUDES
+#include "game_tipos.h"
+#include "game_funcionamiento.h"
+
+/* variables privadas ------------------------*/
+ADC_HandleTypeDef hadc1; 
+
+Game_Handle_t hGame;             // la instancia 
+
+/* Private user code ---------------------------------------------------------*/
+
+int main(void)
+{
+  HAL_Init();
+  SystemClock_Config();
+  
+  MX_GPIO_Init(); //falta por hacer las configuraciones de pines <<<<----------
+  MX_ADC1_Init(); 
+
+  Game_Init(&hGame);
+
+  while (1)
+  {
+    // LEER SENSORES 
+    HAL_ADC_Start(&hadc1);
+    if (HAL_ADC_PollForConversion(&hadc1, 10) == HAL_OK) {
+        uint32_t rawValue = HAL_ADC_GetValue(&hadc1);
+        
+        //  ACTUALIZAR LÓGICA
+        Game_Update(&hGame, rawValue);
+    }
+
+    // ACTUALIZAR ACTUADORES (leds)
+ 
+    // Mostrar progreso en 3 LEDs nuestros
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, (hGame.currentDigitIndex > 0) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, (hGame.currentDigitIndex > 1) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7, (hGame.currentDigitIndex > 2) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+
+    // Feedback de estado ya sea Ganar/Perder
+    if(hGame.currentState == STATE_WIN) {
+        // Parpadeo verde
+        HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0); 
+        HAL_Delay(200); // Bloqueo temporal
+    } 
+    else if (hGame.currentState == STATE_LOSE) {
+        // Parpadeo Rojo rápido + Buzzeador
+        HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_1);
+        HAL_Delay(50);
+    }
+
+    HAL_Delay(10); //por si aca
+  }
+}
